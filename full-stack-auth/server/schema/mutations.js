bad41ed2b65fd157d53f178db26c00c58b61a0a9
Dean Glueckler <glueckler@gmail.com>
2018-11-07 14:43:30 -0800

    login and logout mutations

--- full-stack-auth/server/schema/mutations.js
+++ full-stack-auth/server/schema/mutations.js
@@ -26,6 +26,24 @@ const mutation = new GraphQLObjectType({
           req: request
         });
       }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, args, request) {
+        return AuthService.login({ email: args.email, password: args.password, req: request });
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, request) {
+        const { user } = request;
+        request.logout();
+        return user;
+      }
     }
   }
 });
