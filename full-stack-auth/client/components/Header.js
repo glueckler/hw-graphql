3bc28fab04001e874e7db11e63ad3fe1f403a782
Dean Glueckler <glueckler@gmail.com>
2018-11-07 16:31:09 -0800

    added logout mutation

--- full-stack-auth/client/components/Header.js
+++ full-stack-auth/client/components/Header.js
@@ -1,16 +1,49 @@
 import React, { PureComponent } from 'react'
 import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
 import CurrentUser from '../queries/CurrentUser'
+import Logout from '../mutations/Logout'
 
 class Header extends PureComponent {
-  render() {
-    console.log(this.props.data)
+  handleLogout() {
+    this.props.mutate({
+      refetchQueries: [{ query: CurrentUser }],
+    })
+  }
+
+  renderButtons() {
+    const { loading, user } = this.props.data
+    if (loading) {
+      return <div />
+    }
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.handleLogout.bind(this)}>Logout</a>
+        </li>
+      )
+    }
     return (
       <div>
-        I'm the Header
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">SignUp</Link>
+        </li>
       </div>
+    )
+  }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/">Home</Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
+      </nav>
     )
   }
 }
 
-export default graphql(CurrentUser)(Header)
+export default graphql(Logout)(graphql(CurrentUser)(Header))
